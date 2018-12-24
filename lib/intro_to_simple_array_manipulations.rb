1144b483fd70457fdfde8f4e3661c4f5305e43d0
jameseclement <jameseclement@gmail.com>
2018-12-24 19:21:36 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -22,8 +22,8 @@ def shift_with_args(array)
   array.shift(2)
 end
 
-def using_push(array, string)
-  array.push(string)
+def using_concat(array, array2)
+  array.concat(array2)
 end
 
 def using_push(array, string)
